89890c56404cc38ccf3bd8efbc178b81bffd02c0
Shuric-git <ulme@rambler.ru>
2022-09-06 09:34:59 +0300

    test

--- src/NewsList.tsx
+++ src/NewsList.tsx
@@ -21,7 +21,6 @@ export const NewsList = () => {
             {blockRandomizer()}
             {blockRandomizer()}
             {blockRandomizer()}
-            {blockRandomizer()}
         </div>
     );
 };
